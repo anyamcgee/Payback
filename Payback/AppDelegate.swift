e0997782a624d64c2cc02e43bcd6115ce8dc561a
anyamcgee <anyamcgee@gmail.com>
2015-11-22 19:50:47 -0800

    Added code to register with Data, hopefully added keys to gitignore

--- Payback/AppDelegate.swift
+++ Payback/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        IBMBluemix.initializeWithApplicationId(APPLICATION_ID, andApplicationSecret: APPLICATION_SECRET, andApplicationRoute: APPLICATION_ROUTE)
+        
+        
         return true
     }
 
--- Payback/ViewController.swift
+++ Payback/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import IBMBluemix
 
 class ViewController: UIViewController {
 
