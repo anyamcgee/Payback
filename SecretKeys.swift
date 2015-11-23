861fde511adb7bde36b6827c8cee135265cfb6f7
anyamcgee <anyamcgee@gmail.com>
2015-11-22 20:14:39 -0800

    Please don't add the goddamn key file

---
+++ SecretKeys.swift
@@ -0,0 +1,13 @@
+//
+//  SecretKeys.swift
+//  Payback
+//
+//  Created by Anya McGee on 2015-11-22.
+//  Copyright © 2015 Stacks of Cache. All rights reserved.
+//
+
+import Foundation
+
+let APPLICATION_ID: String = ""
+let APPLICATION_SECRET: String = ""
+let APPLICATION_ROUTE: String = ""
