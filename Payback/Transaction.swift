05b5677894084c32c37068aea38ddb856a9266cb
Alice Fredine <alice.fredine@gmail.com>
2015-12-12 17:27:41 -0800

    Added transaction table view stuff but it is not pretty yet

--- Payback/Transaction.swift
+++ Payback/Transaction.swift
@@ -13,11 +13,22 @@ class Transaction : IBMDataObject, IBMDataObjectSpecialization {
     // User Receiving Money
     @NSManaged var to: User
     
+    // Email of User Receiving Money
+    @NSManaged var toEmail: String
+    
     // User Paying Money
     @NSManaged var from: User
     
+    
+    // Email of User Paying Money
+    @NSManaged var fromEmail: String
+    
     // amount paid
     @NSManaged var amount: Float
+    
+    // description of transaction
+    @NSManaged var reason: String?
+
     
     // MARK:- Setup
     required override init() {
@@ -33,13 +44,26 @@ class Transaction : IBMDataObject, IBMDataObjectSpecialization {
         return "Transaction"
     }
     
-    class func CreateTransaction(to: User, from: User, amount: Float) {
+    class func CreateTransaction(to: User, from: User, amount: Float, description: String?){
         let transaction = Transaction();
         transaction.to = to
-        transaction.from = to
+        transaction.toEmail = to.email
+        transaction.from = from
+        transaction.fromEmail = from.email
         transaction.amount = amount
+        transaction.reason = description
         transaction.save()
     }
-
     
+    class func SaveFakeData()
+    {
+        let user: User = CurrentUser.sharedInstance.currentUser!
+        self.CreateTransaction(user, from: user, amount: 10.46, description: "Sushi")
+        self.CreateTransaction(user, from: user, amount: 40.00, description: "AirBnb")
+        self.CreateTransaction(user, from: user, amount: 52.50, description: "Christmas Present")
+        self.CreateTransaction(user, from: user, amount: 8.00, description: "Registration Fee")
+        self.CreateTransaction(user, from: user, amount: 10.00, description: nil)
+        self.CreateTransaction(user, from: user, amount: 5.00, description: "Coffee")
+        
+    }
 }
