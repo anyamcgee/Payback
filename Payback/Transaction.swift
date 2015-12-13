45c74e221888022ead5b6aaea0a6279f965d66ad
Alice Fredine <alice.fredine@gmail.com>
2015-12-12 19:10:07 -0800

    Added some stylz to the transactions stuff and bug fixes

--- Payback/Transaction.swift
+++ Payback/Transaction.swift
@@ -14,14 +14,19 @@ class Transaction : IBMDataObject, IBMDataObjectSpecialization {
     @NSManaged var to: User
     
     // Email of User Receiving Money
-    @NSManaged var toEmail: String
+    @NSManaged var toUserEmail: String
+    
+    // Name of User Receiving Money
+    @NSManaged var toUserName: String
     
     // User Paying Money
     @NSManaged var from: User
     
-    
     // Email of User Paying Money
-    @NSManaged var fromEmail: String
+    @NSManaged var fromUserEmail: String
+
+    // Name of User Paying Money
+    @NSManaged var fromUserName: String
     
     // amount paid
     @NSManaged var amount: Float
@@ -47,9 +52,11 @@ class Transaction : IBMDataObject, IBMDataObjectSpecialization {
     class func CreateTransaction(to: User, from: User, amount: Float, description: String?){
         let transaction = Transaction();
         transaction.to = to
-        transaction.toEmail = to.email
+        transaction.toUserEmail = to.email
+        transaction.toUserName = to.name
         transaction.from = from
-        transaction.fromEmail = from.email
+        transaction.fromUserEmail = from.email
+        transaction.fromUserName = from.name
         transaction.amount = amount
         transaction.reason = description
         transaction.save()
@@ -57,13 +64,24 @@ class Transaction : IBMDataObject, IBMDataObjectSpecialization {
     
     class func SaveFakeData()
     {
-        let user: User = CurrentUser.sharedInstance.currentUser!
-        self.CreateTransaction(user, from: user, amount: 10.46, description: "Sushi")
-        self.CreateTransaction(user, from: user, amount: 40.00, description: "AirBnb")
-        self.CreateTransaction(user, from: user, amount: 52.50, description: "Christmas Present")
-        self.CreateTransaction(user, from: user, amount: 8.00, description: "Registration Fee")
-        self.CreateTransaction(user, from: user, amount: 10.00, description: nil)
-        self.CreateTransaction(user, from: user, amount: 5.00, description: "Coffee")
+        let fromUser: User = CurrentUser.sharedInstance.currentUser!
+        let toUser = User()
+        toUser.email = "a.fake.email@email.com"
+        toUser.name = "A Fake Name"
+        toUser.id = "12345"
+        self.CreateTransaction(toUser, from: fromUser, amount: 10.46, description: "Sushi")
+        self.CreateTransaction(toUser, from: fromUser, amount: 40.00, description: "AirBnb")
+        self.CreateTransaction(toUser, from: fromUser, amount: 52.50, description: "Christmas Present")
+        self.CreateTransaction(toUser, from: fromUser, amount: 8.00, description: "Registration Fee")
+        self.CreateTransaction(toUser, from: fromUser, amount: 10.00, description: nil)
+        self.CreateTransaction(toUser, from: fromUser, amount: 5.00, description: "Coffee")
+        self.CreateTransaction(fromUser, from: toUser, amount: 10.46, description: "Sushi")
+        self.CreateTransaction(fromUser, from: toUser, amount: 40.00, description: "AirBnb")
+        self.CreateTransaction(fromUser, from: toUser, amount: 52.50, description: "Christmas Present")
+        self.CreateTransaction(fromUser, from: toUser, amount: 8.00, description: "Registration Fee")
+        self.CreateTransaction(fromUser, from: toUser, amount: 10.00, description: nil)
+        self.CreateTransaction(fromUser, from: toUser, amount: 5.00, description: "Coffee")
+        
         
     }
 }
